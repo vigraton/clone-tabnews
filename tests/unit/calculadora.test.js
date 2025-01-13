0e7a31af755a3f390c34d87b6de58cca445dcaff
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2025-01-13 12:51:10 +0000

    add past ´unit´

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("somar 5 + 100 deve retornar 105", () => {
     const resultado = calculadora.somar(5,100);
@@ -8,4 +8,9 @@ test("somar 5 + 100 deve retornar 105", () => {
 test("somar 2 + 2 deve retornar 4", () => {
     const resultado = calculadora.somar(2,2);
     expect(resultado).toBe(4);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Erro'", () => {
+    const resultado = calculadora.somar("banana", 100);
+    expect(resultado).toBe("Erro");
 });
