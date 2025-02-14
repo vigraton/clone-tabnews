8251db647e787579df5d135d42ee77d37e6b072f
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2025-02-14 00:28:56 +0000

    fix:prettier

--- infra/database.js
+++ infra/database.js
@@ -7,21 +7,21 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log('Credenciais do Postgres: ', {
+  console.log("Credenciais do Postgres: ", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  })
-  
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch(error) {
+  } catch (error) {
     console.error(error);
     throw error;
   } finally {
