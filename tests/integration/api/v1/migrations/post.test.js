68ae314ee135362a987f9a58642588f4a91c4cfe
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2025-09-03 14:20:04 +0000

    style: format code again

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -13,7 +13,7 @@ describe("POST /api/v1/migrations", () => {
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
-          }
+          },
         );
 
         expect(response1.status).toBe(201);
@@ -28,7 +28,7 @@ describe("POST /api/v1/migrations", () => {
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
-          }
+          },
         );
 
         expect(response2.status).toBe(200);
