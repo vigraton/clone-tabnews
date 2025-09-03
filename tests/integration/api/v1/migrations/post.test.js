4f34423bd01eddb4328f1adbcf95ff63681c6a36
Vivian Graton <117688900+vigraton@users.noreply.github.com>
2025-09-03 14:06:59 +0000

    refactor: use `orchestrator.clear.Database()` in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,8 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
@@ -14,7 +13,7 @@ describe("POST /api/v1/migrations", () => {
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
-          },
+          }
         );
 
         expect(response1.status).toBe(201);
@@ -29,7 +28,7 @@ describe("POST /api/v1/migrations", () => {
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
-          },
+          }
         );
 
         expect(response2.status).toBe(200);
